8a3cf92974addcaa577b34b3e8d37668176e8ba4
Iratxe Etxebarria <66633034+IratxeEtxebarria-MAA@users.noreply.github.com>
2023-03-13 13:02:47 +0100

    Update fmw_get_connect_string.sh

--- app_dr_common/fmw_get_connect_string.sh
+++ app_dr_common/fmw_get_connect_string.sh
@@ -5,7 +5,7 @@
 ## Copyright (c) 2022 Oracle and/or its affiliates
 ## Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-## This script returns the oracle.net.tnsadmin directory that a WLS/SOA/FMW datasource is using if any
+## This script returns  connect string that a WLS/SOA/FMW datasource is using
 ##
 ## Usage:
 ##        ./fmw_get_connect_string.sh DATASOURCE_FILE
@@ -13,7 +13,7 @@
 ## Where:
 ##	DATASOURCE_FILE 	The WLS jdbc datasource file (complete path)
 
-### EXAMPLE: (notice the escapes before $ and ')
+### EXAMPLE: 
 ### ./fmw_get_connect_string.sh  /u01/data/domains/my_domain/config/jdbc/opss-datasource-jdbc.xml 
 
 if [[ $# -eq 1 ]]; then
