195b76d7de79651b5eacfa94797389fe54835eca
Fermin Castro <fermin.castro@oracle.com>
2023-09-20 09:09:44 -0700

    Some updates from Laura

--- kubernetes-maa/removeyamlblock.sh
+++ kubernetes-maa/removeyamlblock.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+
+
+## removeyamlblock.sh script version 1.0.
+##
+## Copyright (c) 2023 Oracle and/or its affiliates
+## Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+##
+
+### This script will "clean-up" a yaml block 
+### The namespaces list is obtained from the directory structure in the TAR
+### Usage:
+###
+###      ./removeyamlblock.sh [YAML_FILE] [ROOT_ITEM] [SUBITEM_TO_REMOVE]
+### Where:
+###     YAML_FILE
+###                     The YAML file that will be modified
+###		ROOT_ITEM
+###			The root context where the subitem is located
+###		SUBITEM_TO_REMOVE
+###			The yaml block under root context that will be removed
+
 export file=$1
 export inrootblock=$2
 export indeleteblock=$3
