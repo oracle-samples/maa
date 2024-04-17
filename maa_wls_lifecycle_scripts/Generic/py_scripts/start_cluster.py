d337ad465a71c0bba273a889851b07a95c47c107
Iratxe Etxebarria <66633034+IratxeEtxebarria-MAA@users.noreply.github.com>
2024-04-17 10:41:22 +0200

    Added stop/start script for non-OCI domains too (#54)
    This is something I have pending for some time.
    
    We only had published stop/start scripts for PaaS domains (SOAMP and WLS for OCI). We use them in FSDR playbooks, they are very easy and simple to use because they rely in some OCI PaaS utils.
    
    Now added also other scripts that can be used in any other custom domain. Also easy to use, but they require some env properties and additional input parameters (e.g. the server name you want to start).

---
+++ maa_wls_lifecycle_scripts/Generic/py_scripts/start_cluster.py
@@ -0,0 +1,22 @@
+"""
+Copyright (c) 2024 Oracle and/or its affiliates  
+Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+"""
+import os
+import sys
+import socket
+
+clusterName = sys.argv[1]
+adminURL = os.getenv('WLS_ADMIN_URL')
+wlsadminconfigfile = os.getenv('WLS_USER_ADMIN_CONFIGFILE')
+wlsadminkeyfile = os.getenv('WLS_USER_ADMIN_KEYFILE')
+
+try:
+  connect(userConfigFile=wlsadminconfigfile, userKeyFile=wlsadminkeyfile, url=adminURL)
+  start(name=clusterName,type='Cluster')
+  disconnect()
+except Exception, e:
+  dumpStack()
+  disconnect()
+  raise Exception('Failed to start weblogic cluster')
+
