76515d1def37fecd2c0644a8abbd4c1c684b0556
Fermin Castro <fermin.castro@oracle.com>
2024-04-24 04:29:12 -0700

    1412 edg (#55)
    * Updated scripts info and manage params
    
    * no SS and certs for lbrs

--- 1412EDG/generate_perdomainCACERTS-ohs.sh
+++ 1412EDG/generate_perdomainCACERTS-ohs.sh
@@ -40,15 +40,15 @@ then
 	
 else
 	echo ""
-    echo "ERROR: Incorrect number of parameters used: Expected 5, got $#"
+    	echo "ERROR: Incorrect number of parameters used: Expected 5, got $#"
 	echo "Provide a list of virtual hosts inside single quotes and a store password"
-    echo ""
-    echo "Usage:"
-    echo "    $0 [WLS_DOMAIN_DIRECTORY] [MW_HOME] [KEYSTORE_HOME] [KEYPASS]"
-    echo ""
-    echo "Example:  "
-    echo "    $0 /u01/oracle/config/domains/soaedg /u01/oracle/products/fmw /u01/oracle/config/keystores mycertkeystorepass123 'ohstvhost1.soaedgexample.com ohstvhost2.soaedgexample.com'"
-    exit 1
+    	echo ""
+    	echo "Usage:"
+    	echo "    $0 [WLS_DOMAIN_DIRECTORY] [MW_HOME] [KEYSTORE_HOME] [KEYPASS]"
+    	echo ""
+    	echo "Example:  "
+    	echo "    $0 /u01/oracle/config/domains/soaedg /u01/oracle/products/fmw /u01/oracle/config/keystores mycertkeystorepass123 'ohstvhost1.soaedgexample.com ohstvhost2.soaedgexample.com'"
+    	exit 1
 fi
 export dt=`date +%y-%m-%d-%H-%M-%S`
 . $WL_HOME/server/bin/setWLSEnv.sh
@@ -63,19 +63,38 @@ cp $KEYSTORE_HOME/appIdentityKeyStore.jks $KEYSTORE_HOME/appIdentityKeyStore.$dt
 
 sed -e 's/xmlns="[^"]*"//g' $ASERVER/config/config.xml >/tmp/config-nons.xml
 
-export teststrt=$(xmllint /tmp/config-nons.xml --xpath  "//frontend-host"  | tr -d '[:space:]' | sed -e 's/<\/frontend-host>/\n/g' | awk -F'<frontend-host>' '{print $2}' |   awk '!x[$0]++' | awk '{print "DNS:"$1}')
+export list_of_fe=$(xmllint /tmp/config-nons.xml --xpath  "//frontend-host"  | tr -d '[:space:]' | sed -e 's/<\/frontend-host>/\n/g' | awk -F'<frontend-host>' '{print $2}' |   awk '!x[$0]++')
+export list_of_fe="${list_of_fe//$'\n'/ }"
+export list_of_ports=$(xmllint /tmp/config-nons.xml --xpath  "//frontend-https-port"  | tr -d '[:space:]' | sed -e 's/<\/frontend-https-port>/\n/g' | awk -F'<frontend-https-port>' '{print $2}' |   awk '!x[$0]++')
+export list_of_ports="${list_of_ports//$'\n'/ }"
 
-export newtotalst="${teststrt//$'\n'/,}"
+#Need to implement verification of matching number of ports and front ends. We will require
+#that every FE uses and https port for this EDG
 
-echo "Resulting SAN DNS= $newtotalst"
+echo "List of front-ends : $list_of_fe"
+echo "List of ports : $list_of_ports"
+num_of_fe=$(echo $list_of_fe | awk '{print NF}')
+declare -A matrix
+for ((j=1;j<=num_of_fe;j++)) do
+	matrix[$j,1]=$(echo $list_of_fe | awk -v count=$j '{print $count}')
+	matrix[$j,2]=$(echo $list_of_ports | awk -v count=$j '{print $count}')
+	matrix[$j,3]=$(echo ${matrix[$j,1]}:${matrix[$j,2]})
+	sanurl+="DNS:${matrix[$j,1]},"
+	echo "Downloading and adding front end cert for ${matrix[$j,3]}"
+	openssl s_client -connect ${matrix[$j,3]} -showcerts </dev/null 2>/dev/null|openssl x509 -outform PEM > $KEYSTORE_HOME/${matrix[$j,3]}.crt
+	keytool -import -file $KEYSTORE_HOME/${matrix[$j,3]}.crt -v -keystore $KEYSTORE_HOME/appTrustKeyStore.jks -alias ${matrix[$j,1]} -storepass $KEYPASS
+
+done
+
+final_sanurl=$(echo $sanurl |sed -e 's/\(,\)*$//g')
+echo "FINAL DNS string : $final_sanurl "
 
 for vhost in ${LIST_OF_OHS_SSL_VIRTUAL_HOSTS}; do
-    echo "Generating certificates..."
-	java utils.CertGen -cn $vhost -keyusagecritical "true" -keyusage "digitalSignature,nonRepudiation,keyEncipherment,keyCertSign,dataEncipherment,keyAgreement" -keyfilepass $KEYPASS -certfile $vhost.cert -keyfile $vhost.key -domain $ASERVER -selfsigned -nosanhostdns -a $newtotalst
+	echo "Generating OHS Virtual Host certificate for $vhost..."
+	java utils.CertGen -cn $vhost -keyusagecritical "true" -keyusage "digitalSignature,nonRepudiation,keyEncipherment,keyCertSign,dataEncipherment,keyAgreement" -keyfilepass $KEYPASS -certfile $vhost.cert -keyfile $vhost.key -domain $ASERVER -nosanhostdns -a $final_sanurl
 	echo "Cleaning previous alias and importing certificates in the Identity store..."
 	keytool -delete -alias $vhost -storepass  $KEYPASS -keystore appIdentityKeyStore.jks
 	java  utils.ImportPrivateKey -certfile $ASERVER/security/$vhost.cert.der -keyfile $ASERVER/security/$vhost.key.der -keyfilepass $KEYPASS -keystore appIdentityKeyStore.jks -storepass $KEYPASS -alias $vhost -keypass $KEYPASS
 done
 
 rm -rf /tmp/config-nons.xml
-
