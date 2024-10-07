758ab7b80aeaeb31ddaf6c930d48235358d858e2
Fermin Castro <fermin.castro@oracle.com>
2024-10-07 12:08:25 +0200

    1412 edg scaleout (#86)
    * Update generate_perdomainCACERTS.sh
    
    * Update generate_perdomainCACERTS-ohs.sh
    
    * Update generate_perdomainCACERTS.sh

--- 1412EDG/generate_perdomainCACERTS-ohs.sh
+++ 1412EDG/generate_perdomainCACERTS-ohs.sh
@@ -6,6 +6,8 @@
 ## Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 ##
 ### This script generates the required "WLS domain CA" certificates for the SSl virtual hosts used by OHS servers.
+### It also downloads and adds certiicates for the different front-end addresses found in the WLS config.xml file.
+### This is done to allow loopbacks and invocations from WLS servers to frontend addresses when these front end addresses are using SSL
 ### This script should be executed in the WLS Admin Server node and in the directory that hosts the Trust and 
 ### Identity Stores used by the WLS domain.
 ### It is expected that the stores have already been create for the WLS domain (using enerate_perdomainCACERTS.sh)
@@ -48,7 +50,7 @@ else
     	echo "    $0 [WLS_DOMAIN_DIRECTORY] [WL_HOME] [KEYSTORE_HOME] [KEYPASS] [LIST_OF_OHS_SSL_VIRTUAL_HOSTS]"
     	echo ""
     	echo "Example:  "
-    	echo "    $0 /u01/oracle/config/domains/soaedg /u01/oracle/products/fmw/wlserver /u01/oracle/config/keystores mycertkeystorepass123 'ohstvhost1.soaedgexample.com ohstvhost2.soaedgexample.com'"
+    	echo "    $0 /u01/oracle/config/domains/soaedg /u01/oracle/products/fmw/wlserver /u01/oracle/config/keystores mycertkeystorepass123 'ohstvhost1.soaedgexample.com:4445 ohstvhost2.soaedgexample.com:4445'"
     	exit 1
 fi
 export dt=`date +%y-%m-%d-%H-%M-%S`
@@ -68,38 +70,77 @@ fi
 
 sed -e 's/xmlns="[^"]*"//g' $ASERVER/config/config.xml >/tmp/config-nons.xml
 
-export list_of_fe=$(xmllint /tmp/config-nons.xml --xpath  "//frontend-host"  | tr -d '[:space:]' | sed -e 's/<\/frontend-host>/\n/g' | awk -F'<frontend-host>' '{print $2}' |   awk '!x[$0]++')
+export list_of_fe=$(xmllint /tmp/config-nons.xml --xpath  "//frontend-host"  | tr -d '[:space:]' | sed -e 's/<\/frontend-host>/\n/g' | awk -F'<frontend-host>' '{print $2}')
 export list_of_fe="${list_of_fe//$'\n'/ }"
-export list_of_ports=$(xmllint /tmp/config-nons.xml --xpath  "//frontend-https-port"  | tr -d '[:space:]' | sed -e 's/<\/frontend-https-port>/\n/g' | awk -F'<frontend-https-port>' '{print $2}' |   awk '!x[$0]++')
+export list_of_ports=$(xmllint /tmp/config-nons.xml --xpath  "//frontend-https-port"  | tr -d '[:space:]' | sed -e 's/<\/frontend-https-port>/\n/g' | awk -F'<frontend-https-port>' '{print $2}')
 export list_of_ports="${list_of_ports//$'\n'/ }"
 
-#Need to implement verification of matching number of ports and front ends. We will require
-#that every FE uses and https port for this EDG
+#Always assuming there is an appropriate config.xml with always a frontend port for every frontend host
+echo "***************************************************************************"
+echo "****** ADDING THE FRONT-END ADDRESSES CERTIFICATES TO THE TRUST STORE******"
+echo "***************************************************************************"
 
 echo "List of front-ends : $list_of_fe"
 echo "List of ports : $list_of_ports"
 num_of_fe=$(echo $list_of_fe | awk '{print NF}')
 declare -A matrix
+
 for ((j=1;j<=num_of_fe;j++)) do
 	matrix[$j,1]=$(echo $list_of_fe | awk -v count=$j '{print $count}')
 	matrix[$j,2]=$(echo $list_of_ports | awk -v count=$j '{print $count}')
 	matrix[$j,3]=$(echo ${matrix[$j,1]}:${matrix[$j,2]})
 	sanurl+="DNS:${matrix[$j,1]},"
-	echo "Downloading and adding front end cert for ${matrix[$j,3]}"
-	openssl s_client -connect ${matrix[$j,3]} -showcerts </dev/null 2>/dev/null|openssl x509 -outform PEM > $KEYSTORE_HOME/${matrix[$j,3]}.crt
-	keytool -import -file $KEYSTORE_HOME/${matrix[$j,3]}.crt -v -keystore $KEYSTORE_HOME/appTrustKeyStore.jks -alias ${matrix[$j,1]} -storepass $KEYPASS
-
+	if [ $(keytool -list -keystore $KEYSTORE_HOME/appTrustKeyStore.jks  -storepass $KEYPASS |  grep ${matrix[$j,3]} | grep -c trustedCertEntry) -ge 1 ]
+	then
+		while true; do
+			read -p "An alias for for frontend ${matrix[$j,3]} already exists in the truststore. Do you want to replace it? " yn
+			case $yn in
+        			[Yy]* ) echo "Removing current cert for ${matrix[$j,3]}"; addtrust=true;keytool  -keystore $KEYSTORE_HOME/appTrustKeyStore.jks  -storepass $KEYPASS  -delete  -noprompt -alias ${matrix[$j,3]};break;;
+        			[Nn]* ) echo "Skipping download and update for ${matrix[$j,3]}";addtrust=false;break ;;
+        			* ) echo "Please answer y or n.";;
+    			esac
+		done
+	else
+		 addtrust=true
+	fi
+	if [ "$addtrust" = true ];then
+		echo "Downloading and adding front end cert for ${matrix[$j,3]}"
+		openssl s_client -connect ${matrix[$j,3]} -showcerts </dev/null 2>/dev/null|openssl x509 -outform PEM > $KEYSTORE_HOME/${matrix[$j,3]}.crt
+		keytool -import -file $KEYSTORE_HOME/${matrix[$j,3]}.crt -v -keystore $KEYSTORE_HOME/appTrustKeyStore.jks -alias ${matrix[$j,3]} -storepass $KEYPASS
+	fi
 done
 
 final_sanurl=$(echo $sanurl |sed -e 's/\(,\)*$//g')
-echo "FINAL DNS string : $final_sanurl "
+
+echo "***************************************************************************"
+echo "******* CREATING AND ADDING CERTIFICATES FOR THE OHS VIRTUAL HOSTS  *******"
+echo "***************************************************************************"
+
+#We qualify alias with port in case we have mutiple certs for different virtuaL host that
+#share the same host but listen on different port
 
 for vhost in ${LIST_OF_OHS_SSL_VIRTUAL_HOSTS}; do
-	echo "Generating OHS Virtual Host certificate for $vhost..."
-	java utils.CertGen -cn $vhost -keyusagecritical "true" -keyusage "digitalSignature,nonRepudiation,keyEncipherment,keyCertSign,dataEncipherment,keyAgreement" -keyfilepass $KEYPASS -certfile $vhost.cert -keyfile $vhost.key -domain $ASERVER -nosanhostdns -a $final_sanurl -validuntil "2030-03-01"
-	echo "Cleaning previous alias and importing certificates in the Identity store..."
-	keytool -delete -alias $vhost -storepass  $KEYPASS -keystore appIdentityKeyStore.jks
-	java  utils.ImportPrivateKey -certfile $ASERVER/security/$vhost.cert.der -keyfile $ASERVER/security/$vhost.key.der -keyfilepass $KEYPASS -keystore appIdentityKeyStore.jks -storepass $KEYPASS -alias $vhost -keypass $KEYPASS
+	 #Check if the cert for this virtual host already exists
+	if [ $(keytool -list -keystore  appIdentityKeyStore.jks -storepass $KEYPASS |  grep $vhost | grep -c PrivateKeyEntry) -ge 1 ]
+	then
+		while true; do
+                       	read -p "An entry for virtual host $vhost already exists in the Identity Store. Do you want to replace it? " yn
+	        	case $yn in
+        		[Yy]* ) echo "Removing current entry for  $vhost"; addcert=true;keytool  -keystore $KEYSTORE_HOME/appIdentityKeyStore.jks  -storepass $KEYPASS  -delete  -noprompt -alias $vhost;break;;
+                	[Nn]* ) echo "Skipping update for  $vhost";addcert=false;break ;;
+                	* ) echo "Please answer y or n.";;
+                        esac
+                	
+		done
+
+	else
+                addcert=true
+        fi
+        if [ "$addcert" = true ];then
+		echo "Generating and adding cert for $vhost"
+        	java utils.CertGen -cn $vhost -keyusagecritical "true" -keyusage "digitalSignature,nonRepudiation,keyEncipherment,keyCertSign,dataEncipherment,keyAgreement" -keyfilepass $KEYPASS -certfile $vhost.cert -keyfile $vhost.key -domain $ASERVER -nosanhostdns -a $final_sanurl -validuntil "2030-03-01"
+        	java  utils.ImportPrivateKey -certfile $ASERVER/security/$vhost.cert.der -keyfile $ASERVER/security/$vhost.key.der -keyfilepass $KEYPASS -keystore appIdentityKeyStore.jks -storepass $KEYPASS -alias $vhost -keypass $KEYPASS
+        fi
 done
 
 rm -rf /tmp/config-nons.xml
