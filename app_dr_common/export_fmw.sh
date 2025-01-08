aea1e404714f19a71ba9face4d1151e8c652322e
Fermin Castro <fermin.castro@oracle.com>
2025-01-08 10:27:01 -0800

    Updated comments and argument management (#103)

--- app_dr_common/export_fmw.sh
+++ app_dr_common/export_fmw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## import_fmw.sh script version 1.0.
+## export_fmw.sh script version 1.0.
 ##
 ## Copyright (c) 2025 Oracle and/or its affiliates
 ## Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
@@ -8,44 +8,60 @@
 ### This script generates a packed extraction of the required tablespaces, schemas and roles used by Oracle FMW
 ### It uses Oracle Data Pump Export and DDL extraction from a PDB hosting a FMW system (typically JRF or SOA domain)
 ### to create a tar that can be transferred to another database system to "migrate" the original FMW domain.
-### It can move a FMW system to a different PDB or a totally different database (ideally in a PDB hosted by a  multitenant database.
+### - It has to be executed in any of the FMW DB nodes.
+### - It can move a FMW system to a different PDB or a totally different database (ideally in a PDB hosted by a  multitenant database).
 ### - It identifies the schemas based on the prefix provided in the Repository Creation Utility (RCU).
 ### - The precise list of schemas exported is configured in the script's variable "schema_list"- The default value provided
 ### for this variable is the on required to export a standard EDG FMW SOA on prem system.
 ### - It requires a tns alias mapping to a service (attached to a single instance in RAC configuration) to conect to the precise PDB.
-### Create an instance-specific service and an alias for it in tnsnames.ora. Pending to be automated. For example:
-### [oracle@drdbrac12a1 ~]$ srvctl add service -db $ORACLE_UNQNAME -service export_soaedg.example.com -preferred  SOADB231 -pdb SOADB23_pdb1
-### [oracle@drdbrac12a1 ~]$ srvctl start service -s  export_soaedg.example.com -db $ORACLE_UNQNAME
-### [oracle@drdbrac12a1 ~]$ lsnrctl status | grep export_soaedg.example.com
-### Service "export_soaedg.example.com" has 1 instance(s).
-### [oracle@drdbrac12a1 ~]$ cat /u01/app/oracle/product/23.0.0.0/dbhome_1/network/admin/tnsnames.ora | grep export
-### EXPORT_SOADB23_PDB1=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=drdbrac12a-scan.dbsubnet.vcnlon80.oraclevcn.com)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=export_soaedg.example.com)(FAILOVER_MODE=(TYPE=select)(METHOD=basic))))
-### - It assumes same password in the PDB for the sys user and schema, but can be easily customized with the schema_pass variable
+### 	Create an instance-specific service and an alias for it in tnsnames.ora. Pending to be automated. For example:
+### 	[oracle@fmwdbnode1 ~]$ srvctl add service -db $ORACLE_UNQNAME -service export_soaedg.example.com -preferred  SOADB231 -pdb SOADB23_pdb1
+### 	[oracle@fmwdbnode1 ~]$ srvctl start service -s  export_soaedg.example.com -db $ORACLE_UNQNAME
+### 	[oracle@fmwdbnode1 ~]$ lsnrctl status | grep export_soaedg.example.com
+### 	Service "export_soaedg.example.com" has 1 instance(s).
+### 	[oracle@fmwdbnode1 ~]$ cat /u01/app/oracle/product/23.0.0.0/dbhome_1/network/admin/tnsnames.ora | grep export
+### 	EXPORT_SOADB23_PDB1=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=drdbrac12a-scan.dbsubnet.vcnlon80.oraclevcn.com)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=export_soaedg.example.com)(FAILOVER_MODE=(TYPE=select)(METHOD=basic))))
+###
+###
 ### Usage:
 ###
-###      	./export_fmw.sh [RCU_PREFIX] [SYS_PASSWORD] [TNS_ALIAS] [EXPORT_DIRECTORY]
+###      	./export_fmw.sh [RCU_PREFIX] [RCU_PASSWORD] [SYS_PASSWORD] [TNS_ALIAS] [EXPORT_DIRECTORY]
 ### Where:
 ###		RCU_PREFIX:
 ###			Prefix used for schemas when the FMW RCU was used to create the Database artifacts used by a FMW domain
+###             RCU_PASSWORD:
+###                     Password provided for schemas when FMW RCU was executed to create the Database artifacts used by a FMW domain.
 ###		SYS_PASSWORD:
-###			User sys's password in the PDB hosting the FMW systems
+###			User sys's password in the PDB hosting the FMW systems.
 ###		TNS_ALIAS:
 ###			Alias in tnsnames.ora that identifies the connect string to be used for the export
 ###		EXPORT_DIRECTORY:
 ###			Directory where exports and ddl will be created
+if [[ $# -eq 5 ]];
+then
+	export dt=`date +%y-%m-%d-%H-%M-%S`
+	export prefix=$1
+	export schema_pass=$2
+	export sys_pass=$3
+	export tns_alias=$4
+	export dumpdir=$5/FMW_EXPORTS_${dt}/
+	export logdir="$dumpdir"
+else
+	echo ""
+    	echo "ERROR: Incorrect number of parameters used: Expected 5, got $#"
+    	echo ""
+    	echo "Usage:"
+    	echo "    $0 [RCU_PREFIX] [RCU_PASSWORD] [SYS_PASSWORD] [TNS_ALIAS] [EXPORT_DIRECTORY]"
+    	echo ""
+    	echo "Example:  "
+    	echo "    $0 FMW1412 myrcupasswd123 mysyspasswd123 EXPORT_SOADB23_PDB1 /u01/dbdataexports "
+    	exit 1
+fi
 
-export dt=`date +%y-%m-%d-%H-%M-%S`
-export prefix=$1
-export passwd=$2
-export tns_alias=$3
-export dumpdir=$4/FMW_EXPORTS_${dt}/
-export logdir="$dumpdir"
-
-export schema_pass=$passwd
-#Schemas para SOA Cloud
+#Schemas for SOA Cloud
 #export schema_list="WLS UMS IAU OPSS ESS SOAINFRA IAU_APPEND IAU_VIEWER WLS_RUNTIME STB MDS DBFS MFT"
 
-#Schemas para SOA on-prem
+#Schemas for SOA on-prem
 export schema_list="WLS UMS IAU OPSS SOAINFRA IAU_APPEND IAU_VIEWER WLS_RUNTIME STB MDS"
 
 export beautify="
@@ -73,7 +89,7 @@ exec DBMS_METADATA.Set_Transform_Param(DBMS_METADATA.SESSION_TRANSFORM, 'SQLTERM
 mkdir -p $dumpdir
 cd $dumpdir
 #SYS generic ops
-sqlplus sys/""${passwd}""@${tns_alias} as sysdba << EOF
+sqlplus sys/""${sys_pass}""@${tns_alias} as sysdba << EOF
 DROP DIRECTORY DUMP_INFRA;
 CREATE DIRECTORY DUMP_INFRA AS '$dumpdir';
 GRANT READ,WRITE ON DIRECTORY DUMP_INFRA TO SYS;
@@ -91,7 +107,7 @@ for schema in $schema_list;do
         real_schema="$prefix"_"$schema"
 	real_schema_list+="$real_schema "
         echo "Updating schema rights for $real_schema..."
-	sqlplus -s sys/""${passwd}""@${tns_alias} as sysdba << EOF
+	sqlplus -s sys/""${sys_pass}""@${tns_alias} as sysdba << EOF
 	GRANT READ,WRITE ON DIRECTORY DUMP_INFRA TO $real_schema;
 	GRANT SELECT ON "SYSTEM"."SCHEMA_VERSION_REGISTRY" TO "${real_schema}";
 	$beautify
@@ -103,7 +119,7 @@ for schema in $schema_list;do
 	spool off
 EOF
 
-	expdp ${real_schema}/"${schema_passwd}"@${tns_alias} schemas=${real_schema} directory=DUMP_INFRA dumpfile=${real_schema}_export.dmp logfile=${real_schema}_export.log PARALLEL=1 CLUSTER=N encryption_password=$passwd;
+	expdp ${real_schema}/"${schema_pass}"@${tns_alias} schemas=${real_schema} directory=DUMP_INFRA dumpfile=${real_schema}_export.dmp logfile=${real_schema}_export.log PARALLEL=1 CLUSTER=N encryption_password=$sys_pass;
 done
 echo "$real_schema_list">${dumpdir}/schema_list.log
 
@@ -122,14 +138,14 @@ directory=DUMP_INFRA
 dumpfile=SYSTEM_SCHEMA_VERSION_REGISTRY.dmp 
 logfile=SYSTEM_SCHEMA_VERSION_REGISTRY.log 
 CLUSTER=N 
-encryption_password="$passwd"
+encryption_password="$sys_pass"
 QUERY=SYSTEM.SCHEMA_VERSION_REGISTRY$:"where OWNER like '${prefix}%'" 
 EOF
-expdp \"sys/"${passwd}"@${tns_alias} as sysdba\" parfile=$dumpdir/sysparam.cfg
+expdp \"sys/"${sys_pass}"@${tns_alias} as sysdba\" parfile=$dumpdir/sysparam.cfg
 
 #Role DDL
 (
-sqlplus -s  sys/""${passwd}""@${tns_alias} as sysdba << EOF
+sqlplus -s  sys/""${sys_pass}""@${tns_alias} as sysdba << EOF
 $beautify
 select DISTINCT role from dba_roles;
 EOF
@@ -138,7 +154,7 @@ EOF
 export roles_list=$(cat $dumpdir/role_list.log)
 
 for role in $roles_list;do
-        sqlplus -s  sys/""${passwd}""@${tns_alias} as sysdba << EOF
+        sqlplus -s  sys/""${sys_pass}""@${tns_alias} as sysdba << EOF
 	$beautify
         spool ${dumpdir}/create_role_${role}.sql
         SELECT DBMS_METADATA.GET_DDL('ROLE','${role}') FROM DUAL;
@@ -153,7 +169,7 @@ done
 # Tablespace DDL
 
 (
-sqlplus -s  sys/""${passwd}""@${tns_alias} as sysdba << EOF
+sqlplus -s  sys/""${sys_pass}""@${tns_alias} as sysdba << EOF
 $beautify
 select DISTINCT tablespace_name from dba_segments WHERE OWNER like '${prefix}%';
 select DISTINCT temporary_tablespace from dba_users WHERE username like '${prefix}%';
@@ -163,7 +179,7 @@ EOF
 export tablespaces_list=$(cat $dumpdir/tablespaces_list.log)
 
 for tablespace in $tablespaces_list;do
-        sqlplus -s  sys/""${passwd}""@${tns_alias} as sysdba << EOF
+        sqlplus -s  sys/""${sys_pass}""@${tns_alias} as sysdba << EOF
 	$beautify
         spool ${dumpdir}/create_tablespace_${tablespace}.sql
         SELECT DBMS_METADATA.GET_DDL('TABLESPACE','${tablespace}') FROM DUAL;
